b39a5d9dbcf9a543665422e62509673e261db833
Lisandro939 <lisandrofernandez2705@gmail.com>
2023-01-05 16:35:35 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function page() {
+  return (
+    <div className='w-screen h-screen justify-center items-center'>
+        <Header />
+    </div>
+  )
+}
